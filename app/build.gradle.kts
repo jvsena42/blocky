801e9714ad640a5095c9a54d648916069ed594d6
Joao Victor Sena <jvsena16@gmail.com>
2025-01-18 15:24:00 -0300

    fix: serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 val MEMPOOL_WS_URL: String by project
--- app/src/main/java/com/github/jvsena42/blocky/domain/repository/BlockRepositoryImpl.kt
+++ app/src/main/java/com/github/jvsena42/blocky/domain/repository/BlockRepositoryImpl.kt
@@ -1,24 +1,41 @@
 package com.github.jvsena42.blocky.domain.repository
 
+import android.util.Log
 import com.github.jvsena42.blocky.data.datasource.WebSocketDataSource
 import com.github.jvsena42.blocky.db.BlockDao
 import com.github.jvsena42.blocky.domain.mapper.toEntity
 import com.github.jvsena42.blocky.domain.mapper.toModel
 import com.github.jvsena42.blocky.domain.model.Block
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class BlockRepositoryImpl(
     private val webSocketDataSource: WebSocketDataSource,
     private val blockDao: BlockDao
 ) : BlockRepository {
 
-    override fun getBlocks(): Flow<List<Block>> {
-        webSocketDataSource.connectToBlockUpdates().onEach { block ->
-            blockDao.insertBlock(block.toEntity())
-            blockDao.deleteOldBlocks()
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            webSocketDataSource.connectToBlockUpdates().onEach { block ->
+                Log.d(TAG, "Received block: $block")
+                blockDao.insertBlock(block.toEntity())
+                blockDao.deleteOldBlocks()
+            }.collect()
         }
+    }
+
+    override fun getBlocks(): Flow<List<Block>> {
+        Log.d(TAG, "getBlocks: ")
+//        webSocketDataSource.connectToBlockUpdates().onEach { block ->
+//            Log.d(TAG, "getBlocks: $block")
+//            blockDao.insertBlock(block.toEntity())
+//            blockDao.deleteOldBlocks()
+//        }
 
         return blockDao.getRecentBlocks()
             .map { entities ->
@@ -28,5 +45,9 @@ class BlockRepositoryImpl(
 
     override suspend fun disconnect() {
         webSocketDataSource.disconnect()
+    }
+
+    companion object {
+        private const val TAG = "BlockRepositoryImpl"
     }
 }
