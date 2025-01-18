21a2160e6145d1da56d33c6907a8ab04204a18fc
Joao Victor Sena <jvsena16@gmail.com>
2025-01-18 15:36:43 -0300

    fix: serialization

--- app/src/main/java/com/github/jvsena42/blocky/data/dto/request/WebSocketMessageDTO.kt
+++ app/src/main/java/com/github/jvsena42/blocky/data/dto/request/WebSocketMessageDTO.kt
@@ -4,16 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-object WebSocketMessageDTO {
-    @Serializable
-    data class Subscribe(
-        @SerialName("action") val action: String = "want",
-        @SerialName("data") val data: List<String>
-    ) {
-        companion object {
-            fun subscribeToBlocks() = Subscribe(data = listOf("blocks"))
-            fun subscribeToTransactions() = Subscribe(data = listOf("transactions"))
-            fun subscribeToAll() = Subscribe(data = listOf("blocks", "transactions"))
-        }
-    }
-}
+data class Subscribe(
+    @SerialName("action") val action: String = "want",
+    @SerialName("data") val data: List<String> = listOf("blocks")
+)
